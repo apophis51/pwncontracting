53ee3bb6e1d5bb40860025bc79ac12678c9d4175
apophis51 <malcolmxvernon@hotmail.com>
2024-11-23 04:11:56 -0500

    Started Site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
+//npm install babel-plugin-react-compiler
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: true,
+  },
+  typescript: {            
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
