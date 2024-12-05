ffc20d122b76a1aa4c4f2bbc76f847a3c09f4b63
apophis51 <malcolmxvernon@hotmail.com>
2024-12-05 04:17:00 -0500

    trying to make blogs work universal

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,9 +1,32 @@
 import ReactMarkdown from 'react-markdown';
 
-export default function Page() {
+import { projectURLS } from '@/pwncontracting.config'
+
+
+async function serverGetBlogs() {
+
+    console.log('about to fetch serverBlogs')
+    const res = await fetch(projectURLS().pythonMongoDBServer)
+    console.log('we just got a res response')
+    const data = await res.json()
+    return data
+}
+
+
+export default async function Page({ params }) {
+
+    const myParamsID = (await params).id
+    console.log("params", myParamsID)
+    const data = await serverGetBlogs()
+    console.log("data", data)
+    console.log(data.find((blog) => blog.id == myParamsID ))
+
+    // const data = serverGetBlogs({ params })
+    // const { id } = await params
+
     return (
-        <div>
-            <h1>OK</h1>
+        <div className="bg-white p-10 px-80 prose prose-md max-w-none">
+            <ReactMarkdown>{data.find((blog) => blog.id == myParamsID ).MarkdownContent}</ReactMarkdown>
         </div>
     );
 }
