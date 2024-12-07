f7243510a59e04d28863215ba44a697d79473f16
apophis51 <malcolmxvernon@hotmail.com>
2024-12-07 05:25:11 -0500

    adding meta data

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -3,14 +3,24 @@ import ReactMarkdown from 'react-markdown';
 import { serverGetBlogsByID } from '@/services/queries';
 
 
+//https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+export async function generateMetadata({ params }: { params: Promise<{ id: string }> }) {  //only suported in server components
+    const myParamsID = await params
+    const data = await serverGetBlogsByID(myParamsID.id)
+    console.log(data)
+
+    return {
+      title: data.Title,
+      // description: data.description
+    }
+  }
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
 
     const myParamsID = await params
     const data = await serverGetBlogsByID(myParamsID.id)
-
-    //
-
+    // const data = await (serverGetBlogsByID(myParamsID.id))
+    // console.log(data.replace(/\s+/g, '-').toLowerCase())
 
     return (
         <div className="bg-white p-10 px-5 xl:px-80 prose prose-md max-w-none">
--- public/components/BlongRenderHorizontal.tsx
+++ public/components/BlongRenderHorizontal.tsx
@@ -14,7 +14,7 @@ interface Blog {
     MarkdownContent: string;
 }
 
-export default function BlogRenderConstructionBlogs({data, linkPath}) {
+export default function BlogRenderConstructionBlogs({ data, linkPath }) {
 
     console.log('attempting to render Horizontal Blogs')
     const scrollContainerRef = useRef<HTMLDivElement>(null);
@@ -52,42 +52,49 @@ export default function BlogRenderConstructionBlogs({data, linkPath}) {
     return (
         // <MainContentTemplate title={"My Construction Blogs"}>
 
-            <div className="">
-                {/* Scrollable Container */}
-                <div className="flex justify-center items-center gap-5 md:gap-12 bg-green-400 px-10">
-                    <button
-                        onClick={scrollLeft}
-                        className="z-10 p-2 bg-gray-800 text-white rounded-full focus:outline-none h-[60px]"
-                    >
-                        &#8249; {/* Left arrow symbol */}
-                    </button>
-                    <div
-                        ref={scrollContainerRef}
-                        className='flex flex-row justify-between items-center gap-2 min-h-[70vh] min-w-[65vw] overflow-x-auto px-4 ' >
-                        {/* <p className='bg-white text-black  '>We Could Not Render Anything </p> */}
-                        {data.map((blog: Blog) => (
-                            <div key={blog.id} className="  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                                <div className="p-5 min-w-[300px] min-h-[50vh] max-w-[50px] max-h-[50px] overflow-y-auto">
-                                    {/* we need to pass the /adminDassh schema in as aprop to make this component even more reusable */}
-                                    <Link href={`${linkPath}/${blog.id}`}> 
+        <div className="">
+            {/* Scrollable Container */}
+            <div className="flex justify-center items-center gap-5 md:gap-12 bg-green-400 px-10">
+                <button
+                    onClick={scrollLeft}
+                    className="z-10 p-2 bg-gray-800 text-white rounded-full focus:outline-none h-[60px]"
+                >
+                    &#8249; {/* Left arrow symbol */}
+                </button>
+                <div
+                    ref={scrollContainerRef}
+                    className='flex flex-row justify-between items-center gap-2 min-h-[70vh] min-w-[65vw] overflow-x-auto px-4 ' >
+                    {/* <p className='bg-white text-black  '>We Could Not Render Anything </p> */}
+                    {data.map((blog: Blog) => (
+                        <div key={blog.id} className="  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-5 min-w-[300px] min-h-[50vh] max-w-[50px] max-h-[50px] overflow-y-auto">
+                                {/* we need to pass the /adminDassh schema in as aprop to make this component even more reusable */}
+                                {/* <Link href={`${linkPath}/${blog.Title.replace(/\s+/g, '-').toLowerCase()}`}> */}
+                                <Link href={`${linkPath}/${blog.id}`}> 
+                                    {/* \s+: Matches one or more whitespace characters.
+g: Global flag ensures all matches are replaced, not just the first one.
+'-': Replaces the matched spaces with dashes.
+For example:
+
+If blog.Title is "My Blog Post", the resulting URL will be /My-Blog-Post. */}
                                     <div className="mb-3 font-normal text-gray-700 dark:text-gray-400 prose prose-sm">
                                         <ReactMarkdown>{blog.MarkdownContent}</ReactMarkdown>
                                     </div>
-                                       </Link>
-                                </div>
+                                </Link>
                             </div>
-                        ))}
-                    </div>
-                    {/* Right Arrow */}
-                    <button
-                        onClick={scrollRight}
-                        className="z-10 p-2 bg-gray-800 text-white rounded-full focus:outline-none h-[60px]"
-                    >
-                        &#8250; {/* Right arrow symbol */}
-                    </button>
+                        </div>
+                    ))}
                 </div>
-
+                {/* Right Arrow */}
+                <button
+                    onClick={scrollRight}
+                    className="z-10 p-2 bg-gray-800 text-white rounded-full focus:outline-none h-[60px]"
+                >
+                    &#8250; {/* Right arrow symbol */}
+                </button>
             </div>
+
+        </div>
         // </MainContentTemplate>
     )
 } 
