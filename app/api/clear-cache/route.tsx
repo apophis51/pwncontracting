d6b239297bc4e368c89fae68ef29a04b550f3239
apophis51 <malcolmxvernon@hotmail.com>
2024-12-07 01:56:10 -0500

    updated clear cache route

--- app/api/clear-cache/route.tsx
+++ app/api/clear-cache/route.tsx
@@ -6,7 +6,5 @@ import { revalidatePath, revalidateTag} from "next/cache"
 export async function GET(request : Request) {
     // revalidatePath("/blog") to revalidate the path
     revalidateTag("blogs")
-    return new Response(JSON.stringify({ message: "Blog Cached Cleared" }), {
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return Response.json({ message: "Blog Cached Cleared" });
   }
